ded3bdd511ae6ba2774e6903109be70ba303a4ea
Daniel Lee <lee@isi-solutions.org>
2015-06-29 22:51:58 +0200

    Boilerplate index template

--- weather_server/show_weather/views.py
+++ weather_server/show_weather/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+"""
+WeatherServer
+Copyright (C) 2015  Full Stack Embedded
 
-# Create your views here.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
+
+def index(request):
+    template = get_template("index.html")
+    variables = Context({
+        "head_title": "Open Weather Station",
+        "page_title": "Open Weather Station",
+        "page_body": "Open Weather Station is the reference project for Full "
+                     "Stack Embedded 2016. We hope you like it!",
+    })
+    output = template.render(variables)
+    return HttpResponse(output)
